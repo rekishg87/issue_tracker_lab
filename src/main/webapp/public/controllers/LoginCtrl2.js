11bb1a8c3058b8a891e4a26bc7f285ca41b54924
Rekish <rekishg87@gmail.com>
2015-11-05 14:05:07 +0100

    Origanized code structure a bit

---
+++ src/main/webapp/public/controllers/LoginCtrl2.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Rekish on 9/24/2015.
+ */
+
+angular.module("LoginMod")
+    .controller("LoginController", ['$scope', '$rootScope', '$localStorage', '$cookies', 'LoginFactory',
+        function($scope, $rootScope, $localStorage, $cookies, LoginFactory) {
+
+            $rootScope.isUserLoggedIn = false;
+            $localStorage.isUserLoggedInStr = $rootScope.isUserLoggedIn;
+
+            if($localStorage.sessionIdStorage == undefined) {
+                console.log("LoginCtrl if SessionStorageUser: " + $localStorage.usernameStr);
+                console.log("LoginCtrl if sessionData: " + $rootScope.sessionData);
+                console.log("LoginCtrl if SessionStorageId: " + $localStorage.sessionIdStorage);
+                $rootScope.isUserLoggedIn = false;
+                // window.location = '#/login';
+            } else if($localStorage.sessionIdStorage == $cookies.get("JSESSIONID")) {
+                console.log("LoginCtrl else SessionStorageUser: " + $localStorage.usernameStr);
+                console.log("LoginCtrl else sessionData: " + $rootScope.sessionData);
+                console.log("LoginCtrl else SessionStorageId: " + $localStorage.sessionIdStorage);
+                $rootScope.isUserLoggedIn = true;
+                window.location = '#/home';
+            }
+
+
+            $scope.login = function() {
+                LoginFactory.login($scope.username, $scope.password, function(response){
+                    if(response.status === 200) {
+                        $rootScope.isUserLoggedIn = true;
+                        $localStorage.usernameStr = $scope.username;
+                        $rootScope.sessionData = $localStorage.usernameStr;
+                        $localStorage.sessionIdStorage = $cookies.get("JSESSIONID");
+                        $rootScope.sessionId = $localStorage.sessionIdStorage;
+                        window.location = '#/home';
+                    } else if(response.status === 403) {
+                        $scope.usernameFailed = $scope.username;
+                        $rootScope.isUserLoggedIn = false;
+                        $scope.showFailedMsg = true;
+                    }
+
+                });
+
+            };
+
+            $scope.signup = function() {
+                window.location = '#/signup';
+            };
+        }]);
